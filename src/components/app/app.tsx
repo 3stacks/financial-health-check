70d09e46ad92fa94657388a87d0fd9954f2653ec
Luke Boyle <me@lukeboyle.com>
2019-09-06 22:40:35 +1000

    chore: add app boilerplate

---
+++ src/components/app/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.style.css';
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <div>
+          hello world
+        </div>
+    )
+  }
+}
